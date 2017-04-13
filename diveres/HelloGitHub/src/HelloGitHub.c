431c2c25e6224e4cc3cc2b65332d09e08a410591
devbox <devbox@Devbox>
2017-04-13 22:28:53 +0200

    Change-Id: Ife1201e27e6828611b2e87efaf4ca24a87487a3a Change-Id: I0000000000000000000000000000000000000000Added Second line

--- diveres/HelloGitHub/src/HelloGitHub.c
+++ diveres/HelloGitHub/src/HelloGitHub.c
@@ -11,9 +11,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void putGreetings()
+{
+	puts("Hello git !")
+}
 int main(void) {
 	puts("!!!Hello Git!!!"); /* prints !!!Hello Git!!! */
 	puts("!!!Hello Git 2!!!"); /* prints !!!Hello Git!!! */
-
+	putGreetings();
 	return EXIT_SUCCESS;
 }
